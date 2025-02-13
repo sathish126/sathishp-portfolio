0c746af49b0e90ec8508cee4a80ecdd110549d37
sathishpandiyan126@gmail.com <sathishpandiyan126@gmail.com>
2025-02-13 22:14:00 +0530

    cook

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,6 +1,8 @@
 import project1 from "../assets/project1.webp";
 import project2 from "../assets/project2.webp";
 import project3 from "../assets/project3.webp";
+import project4 from "../assets/project4.webp";
+import project5 from "../assets/project5.webp";
 import personImage from "../assets/testimonial.webp";
 
 import {
@@ -9,7 +11,6 @@ import {
   RiJavascriptLine,
   RiReactjsLine,
   RiNodeTree,
-  RiPhpLine,
   RiBootstrapLine,
   
 } from "@remixicon/react";
@@ -28,7 +29,7 @@ export const NAVIGATION_LINKS = [
 
 export const PROFILE = {
   name: "Sathish P",
-  role: "Frontend Developer",
+  role: "Developer",
   subheading:
     "Passionate coder with 1+ year of experience in web/app development and robotics kits. Freelance developer, focused on delivering innovative solutions, constantly learning and improving to tackle new challenges.",
 };
@@ -36,32 +37,45 @@ export const PROFILE = {
 export const PROJECTS = [
   {
     id: 1,
-    title: "Service Company Website",
+    title: "Service Company Platform",
     description:
-      "A website that offers innovative solutions, focusing on user-friendly design and responsive functionality to enhance user experiences and meet modern needs.",
+      "Developed a comprehensive website for a service company, implementing innovative solutions with a focus on user-centric design. The platform features responsive layouts and intuitive functionality, significantly enhancing user experience and engagement.",
     techStack: ["HTML", "Tailwind CSS"],
     imgSrc: project1,
   },
   {
     id: 2,
-    title: "Portfolio Website",
+    title: "Portfolio",
     description:
-      "A personal portfolio website showcasing projects and skills. Fully responsive and optimized for performance.",
-    techStack: ["React", "Tailwind CSS","Javascript"],
+      "Designed and implemented a personal portfolio website to showcase professional projects and skills. The site is fully responsive, optimized for performance, and features a clean, modern design that effectively highlights my work and capabilities.",
+    techStack: ["React", "Tailwind CSS", "Javascript"],
     imgSrc: project2,
   },
   {
     id: 3,
-    title: "Class Resource Website",
+    title: "Class Resource Platform",
     description:
-      "A resource website for my department, designed to provide students with easy access to educational materials, important updates, and helpful resource",
+      "Created a dedicated resource website for my academic department, streamlining access to educational materials, course updates, and supplementary resources. This platform enhances the learning experience by providing a centralized hub for students to access critical information.",
     techStack: ["HTML", "Tailwind CSS"],
     imgSrc: project3,
   },
- 
-  
+  {
+    id: 4,
+    title: "Department Fest Platform",
+    description:
+      "Solely designed and developed the official website for 'Prodothon'25', our department's annual technical festival. The platform showcases event details, facilitates registration, and serves as the primary information hub for participants and sponsors, enhancing the overall event experience.",
+    techStack: ["HTML", "React JS", "Tailwind CSS"],
+    imgSrc: project4,
+  },
+  {
+    id: 5,
+    title: "College Fest Platform",
+    description:
+      "Engineered an engaging countdown page for 'Kriya 25', our college's flagship technical symposium. The dynamic and interactive design builds anticipation and effectively communicates key event information, contributing to increased participant engagement and event awareness.",
+    techStack: ["HTML", "Angular JS", "Tailwind CSS"],
+    imgSrc: project5,
+  },
 ];
-
 export const SKILLS = [
   {
     name: "HTML",
@@ -89,10 +103,6 @@ export const SKILLS = [
     icon: <RiCodeSSlashLine className="text-teal-400" />,
   },
   {
-    name: "PHP",
-    icon: <RiPhpLine className="text-pink-500" />,
-  },
-  {
     name: "Bootstrap",
     icon: <RiBootstrapLine className="text-indigo-500" />,
   },
@@ -101,11 +111,11 @@ export const SKILLS = [
 export const EXPERIENCES = [
   {
     yearRange: "2024 - Present",
-    role: "Founder & Developer",
+    role: "Developer",
     company: "THE FIXERS",
     description:
       "At The Fixers, I work as a Frontend Developer, handling client-based projects by creating responsive and visually appealing websites to meet client needs and enhance user experience.",
-    techStack: ["Javascript", "React.js", "Next.js", "MongoDB","HTML", "CSS", "MySQL"],
+    techStack: ["Javascript", "React.js", "Next.js",,"HTML", "CSS","Angular" , "Tailwind CSS" ],
   },
   {
     yearRange: "2024 - Present",
@@ -113,7 +123,7 @@ export const EXPERIENCES = [
     company: "SU-PSG TECH",
     description:
       "I worked as a Frontend Developer in the Students Union Web Development Team, focusing on creating responsive and user-friendly websites to enhance the student experience.",
-    techStack: ["HTML", "CSS","Javascript", "React.js", "Next.js"],
+      techStack: ["Javascript", "React.js", "Next.js",,"HTML", "CSS","Angular" , "Tailwind CSS" ],
   },
   
 ];
